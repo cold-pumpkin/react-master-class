21bb712d3d2c0e6c2ad9217ecf3f1ba6b71583f3
Philip <pch4623@gmail.com>
2022-02-20 12:45:48 +0900

    nested route 활용하여 가격 및 차트 탭 셋팅

--- react-masterclass/crpyto-tracker/src/routes/Coin.tsx
+++ react-masterclass/crpyto-tracker/src/routes/Coin.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
-import { useLocation, useParams } from "react-router";
+import { Routes, Route, useLocation, useParams } from "react-router";
 import styled from "styled-components";
+import Chart from "./Chart";
+import Price from "./Price";
 
 const Title = styled.h1`
   font-size: 48px;
@@ -23,6 +25,30 @@ const Header = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
 `;
 
 interface RouterState {
@@ -104,15 +130,52 @@ function Coin() {
       
       setInfo(infoData);
       setPriceInfo(priceData);
+      setLoading(false);
     })(); // 즉시 실행
-  }, []);
+  }, [coinId]);   // 첫 렌더링 시에만 위 코드가 동작하기를 원하지만, 변할 일 없으니 ok
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loading..."}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Routes>
+            <Route path={"/price"} element={<Price/>} />
+            <Route path={"/chart"} element={<Chart/>} />
+          </Routes>
+        </>
+      )}
     </Container>
   );
 }
