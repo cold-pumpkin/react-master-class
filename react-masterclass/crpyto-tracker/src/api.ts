d9875e529dc9f15729e29073812a854108fb6596
Philip <pch4623@gmail.com>
2022-02-21 22:19:05 +0900

    api 호출 함수 분리

--- react-masterclass/crpyto-tracker/src/api.ts
+++ react-masterclass/crpyto-tracker/src/api.ts
@@ -1,5 +1,6 @@
+const BASE_URL = `https://api.coinpaprika.com/v1`;
 export async function fetchCoins() {
-  return await (await fetch("https://api.coinpaprika.com/v1/coins")).json();
+  return await (await fetch(`${BASE_URL}/coins`)).json();
 }
 
 /*
@@ -8,3 +9,13 @@ export function fetchCoins() {
     .then(response => response.json());
 }
 */
+
+export function fetchCoinInfo(coinId: string) {
+  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) => 
+    response.json());
+}
+
+export function fetchCoinTickers(coinId: string) {
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) => 
+    response.json());
+}
